60b93e4e688dbe72ecea33f2f21d77491f6c6ac6
Mohamed Halloud <mohamedhalloud97@gmail.com>
2026-09-06 14:51:23 +0100

    add compute_bias() in eavlute.py

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -13,3 +13,6 @@ def mase(mae_model: float, naive_scale: float) -> float:
     Mean Absolute Scaled Error.
     """
     return mae_model / naive_scale
+
+def compute_bias(y_true: pd.Series, y_pred: pd.Series) -> float:
+    return (y_pred - y_true).mean()
